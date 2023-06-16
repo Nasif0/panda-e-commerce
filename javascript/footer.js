8506923fcbf9a9bfc76df6bfbd08699a49c2b3d0
Nasif.0 <princenasif.00@gmail.com>
2023-06-17 05:01:07 +0600

    yes commit

---
+++ javascript/footer.js
@@ -0,0 +1,36 @@
+
+// watch
+function watchFunction() {
+         document.getElementById("demo-watch").style.color = "white";
+
+ }
+ 
+
+//  bag
+function bagFunction(){
+     document.getElementById('demo-bag').style.color='white';
+}
+
+// shoes
+function shoesFunction(){
+     document.getElementById('demo-shoes').style.color='white';
+}
+
+// main-2 (shoes1)
+function shoes1Function() {
+     document.getElementById("shoes-price1").innerHTML = "$1289";
+   }
+
+// shoes2
+function shoes2Function() {
+     document.getElementById("shoes-price2").innerHTML = "$289";
+   }
+//    shoes 3
+
+function shoes3Function() {
+     document.getElementById("shoes-price3").innerHTML = "$890";
+   }
+
+// main 2 end------
+
+
