e404e1230c0f62eb6f0d852f4ab5fb91aed1da90
Nasif.0 <princenasif.00@gmail.com>
2023-06-17 17:41:00 +0600

    yes commit

---
+++ javascript/all.js
@@ -0,0 +1,91 @@
+
+// watch
+
+
+function watchMouseEnter() {
+     document.getElementById("demo-watch").style.color = "white";
+   }
+   
+   function watchMouseLeave() {
+     document.getElementById("demo-watch").style.color = "black";
+   }
+
+
+
+//  bag
+function bagMouseEnter() {
+     document.getElementById("demo-bag").style.color = "white";
+   }
+   
+   function bagMouseLeave() {
+     document.getElementById("demo-bag").style.color = "black";
+   }
+
+// shoes
+function shoesMouseEnter() {
+     document.getElementById("demo-shoes").style.color = "white";
+   }
+   
+   function shoesMouseLeave() {
+     document.getElementById("demo-shoes").style.color = "black";
+   }
+
+// main-2 (shoes1)
+function shoes1Function() {
+     document.getElementById("shoes-price1").innerHTML = "$1289";
+   }
+
+// shoes2
+function shoes2Function() {
+     document.getElementById("shoes-price2").innerHTML = "$289";
+   }
+//    shoes 3
+
+function shoes3Function() {
+     document.getElementById("shoes-price3").innerHTML = "$890";
+   }
+
+// ------------main 2 end------
+
+// ------------Main 1 Hover start----------
+function bagPack1Function() {
+     document.getElementById("bagPack-price1").innerHTML = "$890";
+   }
+
+
+   
+function beforeWatchBody(x) {
+     x.style.height = "120px";
+     x.style.width = "360px";
+   }
+   
+function normalWatchBody(x) {
+     x.style.height = "120px";
+     x.style.width = "359px";
+   }
+   
+   
+   
+   function beforeBagBody(x) {
+     x.style.height = "120px";
+     x.style.width = "360px";
+   }
+   
+function normalBagBody(x) {
+     x.style.height = "120px";
+     x.style.width = "359px";
+   }
+
+
+   function beforeShoesBody(x) {
+     x.style.height = "120px";
+     x.style.width = "360px";
+   }
+   
+function normalShoesBody(x) {
+     x.style.height = "120px";
+     x.style.width = "359px";
+   }
+//   ----- Main 1 Hover end ------
+
+
